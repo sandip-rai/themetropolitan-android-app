eb9817ba3a40bed2967f1db6b63f7720ddf051e7
Unknown <sandip.sampanG@gmail.com>
2019-02-21 15:53:13 -0600

    Final Version for Elaboration 1 Demo
    Includes Dark Theme

--- TheMetropolitan/app/src/main/java/com/sandiprai/themetropolitan/MainActivity.java
+++ TheMetropolitan/app/src/main/java/com/sandiprai/themetropolitan/MainActivity.java
@@ -4,23 +4,82 @@ import androidx.annotation.NonNull;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+//        if (InitApplication.getInstance().isNightModeEnabled()) {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//        } else {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//        }
         setContentView(R.layout.activity_main);
+
+
+        Switch switchCompat = findViewById(R.id.themeSwitch);
+
+//        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+//            switchCompat.setChecked(true);
+//        }
+
+
+/*        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    InitApplication.getInstance().setIsNightModeEnabled(true);
+                   Intent intent = getIntent();
+                    //intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                   finish();
+ startActivity(intent);
+                } else {
+                   InitApplication.getInstance().setIsNightModeEnabled(false);
+                   Intent intent = getIntent();
+                   //intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                   finish();
+                   startActivity(intent);
+                }
+
+
+            }
+        });*/
+
+//
+
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         TextView toolbarTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
@@ -29,6 +88,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //String appName = String.valueOf(R.string.app_name);
         String appName = "The Metropolitan";
         toolbarTitle.setText(appName.toUpperCase());
+
+//        TabLayout tabLayout = findViewById(R.id.newsTab);
+//        LinearLayout container = findViewById(R.id.fragmentContainerInNews);
+//
+//        tabLayout.addTab(tabLayout.newTab().setText("s"));
+
+
+//        ViewPager viewPager = findViewById(R.id.viewPager);
+//        setupViewPager(viewPager);
+
+//        TabLayout tabLayout = findViewById(R.id.newsTab);
+//        tabLayout.setupWithViewPager(viewPager);
+
 
 //        Fragment fragment = new HelloFragment();
 //        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -47,6 +119,43 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setupBottomNavigationView();
 
     }
+
+//    private void setupViewPager(ViewPager viewPager){
+//        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+//        adapter.addFragment(new TopStoriesFragment(), "Top Stories");
+////        adapter.addFragment(new TwoFragment(), "TWO");
+////        adapter.addFragment(new ThreeFragment(), "THREE");
+//        viewPager.setAdapter(adapter);
+//    }
+
+//    class ViewPagerAdapter extends FragmentPagerAdapter {
+//        private final List<Fragment> mFragmentList = new ArrayList<>();
+//        private final List<String> mFragmentTitleList = new ArrayList<>();
+//
+//        public ViewPagerAdapter(FragmentManager manager) {
+//            super(manager);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int position) {
+//            return mFragmentList.get(position);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return mFragmentList.size();
+//        }
+//
+//        public void addFragment(Fragment fragment, String title) {
+//            mFragmentList.add(fragment);
+//            mFragmentTitleList.add(title);
+//        }
+//
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            return mFragmentTitleList.get(position);
+//        }
+//    }
 
     private void setupBottomNavigationView(){
         BottomNavigationView bottomNavigationView =
@@ -116,9 +225,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
     }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
-    }
+//
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//        return false;
+//    }
 }
--- TheMetropolitan/app/src/main/java/com/sandiprai/themetropolitan/SettingsFragment.java
+++ TheMetropolitan/app/src/main/java/com/sandiprai/themetropolitan/SettingsFragment.java
@@ -3,20 +3,58 @@ package com.sandiprai.themetropolitan;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
 
-public class SettingsFragment extends Fragment {
-
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //THIS PART IS A TEST FOR DARK THEME TESTING
+//    View view = inflater.inflate(R.layout.activity_main, container, false);
+//        Button lightButton = view.findViewById(R.id.button1);
+//        lightButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Code here executes on main thread after user presses button
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//            }
+//        });
+//
+//        Button darkButton = view.findViewById(R.id.button2);
+//        darkButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Code here executes on main thread after user presses button
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//            }
+//        });
+
+//        lightButton.setOnClickListener(this);
+//        darkButton.setOnClickListener(this);
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+        //return view;
     }
 
 
+    @Override
+    public void onClick(View view) {
+//        switch (view.getId()){
+//            case R.id.button1:
+//                //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//            case R.id.button2:
+//                //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//        }
+
+    }
 }
