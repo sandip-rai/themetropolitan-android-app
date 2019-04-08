b545516a347b8e6d5e8df7aa19df170457f3e188
Unknown <sandip.sampanG@gmail.com>
2019-04-07 20:26:29 -0500

    Settings fragment components ready to be implemented

--- TheMetropolitan/app/src/main/java/com/sandiprai/themetropolitan/SettingsFragment.java
+++ TheMetropolitan/app/src/main/java/com/sandiprai/themetropolitan/SettingsFragment.java
@@ -1,12 +1,7 @@
 package com.sandiprai.themetropolitan;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +9,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class SettingsFragment extends Fragment implements View.OnClickListener {
+import com.google.android.material.snackbar.Snackbar;
+
+public class SettingsFragment extends Fragment implements View.OnClickListener,
+        CompoundButton.OnCheckedChangeListener {
 
     private View view;
 
@@ -33,36 +32,30 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         buttonSignIn.setOnClickListener(this);
         buttonSignUp.setOnClickListener(this);
 
-        //using AppCompatDelegate with a switch in android; still working
-//        Switch switchTheme = view.findViewById(R.id.themeSwitch);
-//        switchTheme.setChecked(true);
-//        switchTheme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-//
-//            @Override
-//            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-//                if(isChecked){
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//                    getActivity().recreate();
-//                } else {
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-//                    getActivity().recreate();
-//                }
-//            }
-//        });
-//
-//        Button darkButton = view.findViewById(R.id.button2);
-//        darkButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                // Code here executes on main thread after user presses button
-//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//            }
-//        });
+        //using AppCompatDelegate with a switch in android; still not working
 
+        //Set listener to the switches; see the onCheckChangedListener method below
+        Switch switchTheme = view.findViewById(R.id.themeSwitch);
+        switchTheme.setChecked(true);
+        switchTheme.setOnCheckedChangeListener(this);
+
+        Switch switchNotification = view.findViewById(R.id.notificationSwitch);
+        switchNotification.setChecked(false);
+        switchNotification.setOnCheckedChangeListener(this);
+
+        //Setting listener to textviews
+        TextView fontSizeOption = view.findViewById(R.id.fontSizeView);
+        TextView reportBugsOption = view.findViewById(R.id.reportBugsView);
+        TextView aboutOption = view.findViewById(R.id.aboutView);
+        fontSizeOption.setOnClickListener(this);
+        reportBugsOption.setOnClickListener(this);
+        aboutOption.setOnClickListener(this);
 
         return view;
     }
 
 
+    //Method that listens for button clicks
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -71,11 +64,58 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
                 showToast(text);
                 break;
                 //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
             case R.id.buttonSignUp:
                 CharSequence text1 = "Sign Up button clicked!";
                 showToast(text1);
+
+                //Snackbar.make(getView().findViewById(R.id.buttonSignUp), text1, Snackbar.LENGTH_SHORT).show();
                 break;
                 //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+            case R.id.fontSizeView:
+                CharSequence text2 = "Font size clicked!";
+                showToast(text2);
+                break;
+
+            case R.id.reportBugsView:
+                CharSequence text3 = "Report bugs clicked!";
+                showToast(text3);
+                break;
+
+            case R.id.aboutView:
+                CharSequence text4 = "About clicked!";
+                showToast(text4);
+                break;
+        }
+
+    }
+
+    //Method that listens for switches clicks
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case R.id.themeSwitch:
+                if(isChecked){
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                    getActivity().recreate();
+                    ((MainActivity)getActivity()).themeDarkOn();
+                } else {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                    getActivity().recreate();
+                    ((MainActivity)getActivity()).themeLightOn();
+                }
+                break;
+            case R.id.notificationSwitch:
+                //implement the code implementation here by replacing these
+                if(isChecked){
+                    CharSequence text1 = "Notification Switch On!";
+                    showToast(text1);
+                } else{
+                    CharSequence text2 = "Notification Switch Off!";
+                    showToast(text2);
+                }
+                break;
         }
 
     }
