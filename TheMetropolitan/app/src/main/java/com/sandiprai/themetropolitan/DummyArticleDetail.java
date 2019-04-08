4300b3edf92ec74ce7a8e0ab755c354030cd9c6f
Unknown <sandip.sampanG@gmail.com>
2019-04-08 01:11:46 -0500

    Article button's methods implemented.

--- TheMetropolitan/app/src/main/java/com/sandiprai/themetropolitan/DummyArticleDetail.java
+++ TheMetropolitan/app/src/main/java/com/sandiprai/themetropolitan/DummyArticleDetail.java
@@ -6,12 +6,15 @@ import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DummyArticleDetail extends AppCompatActivity {
 
     public static final String EXTRA_ARTICLE_ID ="articleId";
+    private String articleTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,7 @@ public class DummyArticleDetail extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         int articleId = (int) getIntent().getExtras().get(EXTRA_ARTICLE_ID);
-        String articleTitle = DummyArticle.articles[articleId].getArticleTitle();
+        articleTitle = DummyArticle.articles[articleId].getArticleTitle();
         String articleContent = DummyArticle.articles[articleId].getArticleContent();
         int articleImage = DummyArticle.articles[articleId].getImageId();
 
@@ -45,5 +48,34 @@ public class DummyArticleDetail extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_article,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.like_article:
+                CharSequence text1 = articleTitle + " liked!";
+                showToast(text1);
+                return true;
+            case R.id.save_article:
+                CharSequence text2 = articleTitle + " saved!";
+                showToast(text2);
+                return true;
+            case R.id.share_article:
+                CharSequence text3 = articleTitle + " shared!";
+                showToast(text3);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Shows the passed string as a toast
+    public void showToast(CharSequence text) {
+        int duration1 = Toast.LENGTH_SHORT;
+
+        Toast toast1 = Toast.makeText(getApplicationContext(), text, duration1);
+        toast1.show();
     }
 }
